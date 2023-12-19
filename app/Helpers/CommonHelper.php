b30caf4d9c28943c65c78d6d16b33137dc5405e1
tulparstudyo <teknovipyazilim@gmail.com>
2023-12-19 19:43:51 +0300

    log

--- app/Helpers/CommonHelper.php
+++ app/Helpers/CommonHelper.php
@@ -231,12 +231,13 @@ function addPaymentLog($payment_type, $response, $order, $basket, $order_id=0 ){
 
     }
 }
-function addFailedLog($module, $data=[] ){
+function addFailedLog($module, $message='', $data=[] ){
     $id = 0;
     try {
         $data = json_encode($data, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
         $id = DB::table('failed_logs')->insertGetId([
             'module'=> $module,
+            'message'=>$message,
             'data'=>$data,
         ]);
     } catch (\Exception $exception){
